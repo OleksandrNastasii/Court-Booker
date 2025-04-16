6e84625489029add024946ceda45fc5265b775f7
OleksandrNastasii <SashaNastasiy1@gmail.com>
2025-04-16 16:41:24 +0200

    update: Switched from SQLite to PostgreSQL

--- project/app/database/database.py
+++ project/app/database/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
-from ..config.config import DB_route
+#from ..config.config import DB_route
 
-engine = create_engine(f"{DB_route.DATABASE_URI}")
+engine = create_engine(f"postgresql://user:password@db:5432/mydatabase")
 db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
 
 Base = declarative_base()
