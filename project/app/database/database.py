c20d17631d2a8565bbdc2b9a422779a0ef760c4b
OleksandrNastasii <SashaNastasiy1@gmail.com>
2025-04-16 17:08:27 +0200

    bugfix: assigned proper env variables

--- project/app/database/database.py
+++ project/app/database/database.py
@@ -1,9 +1,18 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
+load_dotenv()
 #from ..config.config import DB_route
-
-engine = create_engine(f"postgresql://user:password@db:5432/mydatabase")
+print(os.getenv("DATABASE_HOST"))
+engine = create_engine("postgresql://{user}:{password}@{db_host}:5432/{database}".format(
+    user = os.getenv("POSTGRESQL_USER"),
+    password = os.getenv("POSTGRESQL_PASSWORD"),
+    database = os.getenv("POSTGRESQL_DATABASE"),
+    db_host = os.getenv("DATABASE_HOST")
+))
 db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
 
 Base = declarative_base()
