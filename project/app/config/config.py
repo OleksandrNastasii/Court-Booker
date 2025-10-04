3b77c4b4d0ff5c46f3b01c184c83d0ca1b20ae44
OleksandrNastasii <SashaNastasiy1@gmail.com>
2025-10-04 18:35:14 +0200

    Update: updated config.py and dashboards.py

--- project/app/config/config.py
+++ project/app/config/config.py
@@ -1,10 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+#Loads data from .env
 load_dotenv()
 
-class DB_route():
-    DATABASE_URI = os.getenv("DATABASE_URI")
-
+#Retrieves Secret Key from .env
 class Secret_Key():
     SECRET_KEY = os.getenv("SECRET_KEY") or "fallback-key"
--- project/app/routes/dashboards.py
+++ project/app/routes/dashboards.py
@@ -2,20 +2,24 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required
 from .role_decorators import admin_required, moderator_required
 
+#Creates Blueprint for dashboards to start at website boot
 dashboard = Blueprint("dashboard", __name__)
 
+#Creates "/admin" endpoint using admin_dashboard.html and sets resritction with access rights
 @dashboard.route('/admin')
 @login_required
 @admin_required
 def admin_dashboard():
     return render_template('admin_dashboard.html')
 
+#Creates "/moderator" endpoint using moderator_dashboard.html and sets resritction with access rights
 @dashboard.route('/moderator')
 @login_required
 @moderator_required
 def moderator_dashboard():
     return render_template('moderator_dashboard.html')
 
+#Redirects user to court selection page
 @dashboard.route('/user')
 @login_required
 def user_dashboard():
