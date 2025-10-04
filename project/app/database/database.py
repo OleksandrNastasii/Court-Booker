a57795cb25727bc90c8d94df18ffe07269aa4beb
OleksandrNastasii <SashaNastasiy1@gmail.com>
2025-10-04 18:28:01 +0200

    Update: updated database.py

--- project/app/database/database.py
+++ project/app/database/database.py
@@ -4,20 +4,30 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
+#Loads variables from .env
 load_dotenv()
-print(os.getenv("DATABASE_HOST"))
+
+#Creates database engine
 engine = create_engine("postgresql://{user}:{password}@{db_host}:5432/{database}".format(
     user = os.getenv("POSTGRESQL_USER"),
     password = os.getenv("POSTGRESQL_PASSWORD"),
     database = os.getenv("POSTGRESQL_DATABASE"),
     db_host = os.getenv("DATABASE_HOST")
 ))
+
+#Creates communication with database using binded engine
 db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
 
+#Base model for databes models
 Base = declarative_base()
+
+#Defines collection of database tables and models for app to use
 metadata = Base.metadata
+
+#Attaches a query property to all model classes
 Base.query = db_session.query_property()
 
+#Creates tables in database using models metadata
 def init_db():
     Base.metadata.create_all(engine)
     
